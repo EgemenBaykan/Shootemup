1437f04537395222a7ae7def0ee2a6a868f835ee
EgemenBaykan <30087171+EgemenBaykan@users.noreply.github.com>
2020-11-22 01:00:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -22,8 +22,11 @@ score = 0
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("SPACE FIGHT")
+pygame.display.set_caption("RedStar 3000")
 clock = pygame.time.Clock()
+
+lastStar = 0
+starTime = 400
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -35,6 +38,8 @@ class Player(pygame.sprite.Sprite):
         self.rect.centerx = 50
         self.rect.bottom = HEIGHT / 2
         self.speedx = 0
+        self.health = 100
+        self.isAlive = False
 
     def update(self):
         self.speedx = 0
@@ -119,32 +124,72 @@ class Boss(pygame.sprite.Sprite):
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.radius = int(self.rect.width * 0.85 / 2)
-        self.rect.x = random.randrange(WIDTH / 1.5, WIDTH - 150)
+        self.rect.x = random.randrange(WIDTH / 1.5, WIDTH - 300)
         self.rect.y = random.randrange(150, HEIGHT - 150)
         self.speedx = random.randrange(-10, 10)
         self.speedy = random.randrange(5, 15)
         self.trackingPlayer = trackingPlayer
         self.isAlive = False
         self.health = 100
+        self.shootTime = random.randrange(300, 800)
+        self.lastShoot = pygame.time.get_ticks()
 
     def update(self):
         self.trackPlayer(self.trackingPlayer)
-        
+        if pygame.time.get_ticks() - self.lastShoot > self.shootTime:
+            self.shoot()
+            self.lastShoot = pygame.time.get_ticks()
         if self.health <= 0:
             self.isAlive = False
             self.kill()            
             drawText(screen, "YOU WON!!!!", 30, WIDTH / 2, HEIGHT / 2)
             drawText(screen, "Your score: " + str(score), 20, WIDTH / 2, HEIGHT / 2 + 30)
             pygame.display.flip()
-            time.sleep(5)
+            time.sleep(3)
 
         
     def trackPlayer(self, player):
-        dx, dy = player.rect.x - self.rect.x, player.rect.y - self.rect.y
+        dx, dy = player.rect.centerx - self.rect.centerx, player.rect.centery - self.rect.centery
         distance = math.hypot(dx + 1, dy + 1)
         dx, dy = dx / distance, dy / distance
-        self.rect.y += dy * self.speedy
-        
+        self.rect.centery += dy * self.speedy
+    
+    def shoot(self):
+        laser = LaserBoss(self.rect.left - 20, self.rect.centery)
+        all_sprites.add(laser)
+        lasers.add(laser)
+
+class LaserBoss(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.flip(laser_img, True, False)
+        self.image = pygame.transform.scale(self.image, (20, 5))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.left = x
+        self.rect.centery = y
+        self.speedx = 40
+
+    def update(self):
+        self.rect.x -= self.speedx
+        if self.rect.left > WIDTH:
+            self.kill()
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(star_img, (15, 2))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.left = x
+        self.rect.centery = y
+        self.speedx = random.randrange(10,20)
+        self.speedy = 10
+
+    def update(self):
+        self.rect.x -= self.speedx
+        if self.rect.left < 0:
+            self.kill()
 
 def drawText(surf, text, size, x, y):
     font = pygame.font.Font(font_name, size)
@@ -163,13 +208,14 @@ def spawnBoss():
     boss.isAlive = True
     all_sprites.add(boss)
     bosses.add(boss)
-    
+    boss.spawnTime = pygame.time.get_ticks()
 
 # Load images
 background = pygame.image.load(path.join(img_dir, "background.png")).convert()
 background_rect = background.get_rect()
 player_img = pygame.image.load(path.join(img_dir, "player.png")).convert()
 laser_img = pygame.image.load(path.join(img_dir, "laser.png")).convert()
+star_img = pygame.image.load(path.join(img_dir, "star.png")).convert()
 
 enemy_images = []
 enemy_list = ['enemy.png', 'enemy2.png', 'enemy3.png']
@@ -178,18 +224,21 @@ for img in enemy_list:
     enemy_images.append(pygame.image.load(path.join(img_dir, img)).convert())
 
 all_sprites = pygame.sprite.Group()
+players = pygame.sprite.Group()
 lasers = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
 bosses = pygame.sprite.Group()
+stars = pygame.sprite.Group()
 
 player = Player()
+player.isAlive = True
 all_sprites.add(player)
+players.add(player)
 
 boss = Boss(player)
 
-for i in range(10):
+for i in range(8):
     spawnEnemy()
-    
 
 # Game loop
 running = True
@@ -203,17 +252,37 @@ while running:
             if event.key == pygame.K_SPACE:
                 player.shoot()
 
+    if pygame.time.get_ticks() - lastStar > starTime:
+        for a in range(100):
+            star = Star(random.randrange(0, WIDTH), random.randrange(0, HEIGHT))
+            all_sprites.add(star)
+            stars.add(star)
+        lastStar = pygame.time.get_ticks()
+
     # Update
     all_sprites.update()
 
     hitsEnemy = pygame.sprite.groupcollide(enemies, lasers, True, True)
     hitsBoss = pygame.sprite.groupcollide(bosses, lasers, False, True)
+    hitsPlayer = pygame.sprite.groupcollide(players, lasers, False, True)
 
-    if score < 100:
+    for hits in hitsPlayer:
+        player.health -= 10
+        if player.health <= 0:
+            player.isAlive = False
+            
+    if player.health <= 0:
+        player.isAlive = False
+
+    if not player.isAlive:
+        drawText(screen, "YOU ARE DEAD LOSER!", 18, WIDTH / 2, HEIGHT / 2)
+        drawText(screen, "Your score: " + str(score), 13, WIDTH / 2, (HEIGHT / 2) + 10)
+        
+    if pygame.time.get_ticks() / 1000 < 30:
         for hit in hitsEnemy:
             score += 10
             spawnEnemy()
-    if score >= 100:
+    elif pygame.time.get_ticks() / 1000 >= 30:
         for enemy in enemies:
             enemy.kill()
             if not bosses:
@@ -221,15 +290,18 @@ while running:
         for hits in hitsBoss:
             boss.health -= 10
 
-        
-
     # Draw / Render
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
-    drawText(screen, "Score: " + str(score), 18, WIDTH / 2, 15)
+
+    if player.isAlive:
+        drawText(screen, "Score: " + str(score), 18, WIDTH / 2, 15)
+        drawText(screen, "Health: " + str(player.health), 18, WIDTH / 3, 15)
+    else:
+        drawText(screen, "YOU ARE DEAD LOSER!!!", 18, WIDTH / 2, HEIGHT / 2)
+        drawText(screen, "Your score: " + str(score), 13, WIDTH / 2, (HEIGHT / 2) + 30)
+        time.sleep(2)
     pygame.display.flip()
-
-
 
 pygame.quit()
